bb666c22e017aec28739d04e552a5af754b3b60e
이정환 (Lee Jeonghwan) <63226383+JH-lee95@users.noreply.github.com>
2022-08-11 15:13:36 +0900

    Create convert_tag_to_name.py

---
+++ convert_tag_to_name.py
@@ -0,0 +1,65 @@
+def convert_tag_to_name(sentence:str,target="#@이름#",name1_list=None,name2_list=None):
+    
+    ## 이름 자동생성기 찾아야 함
+    
+    
+    flag=True
+    position1=["아","이","은"] #받침 o
+    position2=["야","가","는"] #받침 x
+    
+    
+    
+    while flag:
+        
+        start_index=sentence.find(target)
+        end_index=start_index+5
+        
+        if start_index==-1:
+            flag=False
+            
+        else:
+            if sentence[end_index] in position1:
+                
+                name=random.choice(name1_list)
+                last_name=random.choice([True,False])
+                
+                if not last_name:
+                    name=name[1:]
+                
+                print(sentence[end_index])
+                if start_index == 0:
+                    name + sentence[end_index:]
+                else:
+                    sentence=sentence[:start_index] + name + sentence[end_index:]
+                    
+                    
+            elif sentence[end_index] in position2:
+                
+                name=random.choice(name2_list)
+                last_name=random.choice([True,False])
+                
+                if not last_name:
+                    name=name[1:]
+                
+                print(sentence[end_index])
+                if start_index == 0:
+                    sentence=name + sentence[end_index:]
+                else:
+                    sentence=sentence[:start_index] + name + sentence[end_index:]
+                    
+            else:
+                
+                name=random.choice(name1_list+name2_list)
+
+                last_name=random.choice([True,False])
+                
+                if not last_name:
+                    name=name[1:]
+                
+                print(sentence[end_index])
+                sentence=sentence[:start_index] + name + sentence[end_index:]
+            
+                
+                
+    return sentence
+        
